cf59dc0153b97fe790d3482775dcbdd15f2c3823
Zoe-Kim <thfud333@gmail.com>
2021-07-22 01:38:58 +0900

    :hover와 :focus의 스타일 중복 표시 문제 해결
    210722 01:38am
    - :focus 말고 :focus-visible 선택자를 사용하면 키보드에 의한 초점만 선택하기 때문에 마우스 클릭은 선택하지 않는다!!! (https://codepen.io/naradesign/pen/BaRZXXv)

--- js/common.js
+++ js/common.js
@@ -1,32 +1,31 @@
 $(document).ready(function () {
+  
+  // PC 네비게이션 클릭시 이동
+  $('.pc_nav ul li a').on('click', function (e) {
+    e.preventDefault(); // a 태그의 기본 기능 제한!!!
+    const $tg = $( $(this).attr('href') );
 
-  // 스크롤 이벤트 선언
+    $('html, body').stop().animate({scrollTop: $tg.offset().top}, 'slow');
+  });
+  
+  // 스크롤 이벤트
   $(window).on('scroll', function () {
-    // scrollY = 스크롤바의 수직이동거리
     const scrollY = $(this).scrollTop();
-    //console.log(scrollY);
+    console.log(scrollY);
 
-    // 스크롤바의 수직이동거리가 813픽셀 이상이면 .header.active
-    if (scrollY >= 813) $('.header').addClass('active');
+    // .header 상단 고정
+    if (scrollY >= $('#about').offset().top) $('.header').addClass('active');
     else $('.header').removeClass('active');
 
-    // 스크롤바의 수직이동거리가 100픽셀 이상이면  #topBtn에 fadeIn
-    if (scrollY >= 100) $('#topBtn').stop().fadeIn();
-    else $('#topBtn').stop().fadeOut();
+    // #topBtn 등장
+    if (scrollY > 0) $('#topBtn').fadeIn();
+    else $('#topBtn').fadeOut();
   });
-
-  // #topBtn 버튼을 클릭하면 스크롤바를 0으로 만들기
+  
+  // #topBtn 버튼을 클릭시 상단으로 이동
   $('#topBtn').on('click', function () {
     $('html, body').stop().animate({scrollTop: 0}); // html -> ie, body -> 크롬
   });
 
-  // PC 네비게이션 클릭시 상단으로 부드럽게 이동
-  $('.pc_nav ul li a').on('click', function () {
-    const $tg = $( $(this).attr('href') );
-    console.log($tg);
-
-    $('html, body').stop().animate({scrollTop: $tg.offset().top}, 'slow');
-    return false;  // a 태그의 기본 기능 제한!!!
-  });
 
 });
