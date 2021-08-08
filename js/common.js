592a802a808c98a7cab6e24a1e1244ec5af129f5
Zoe-Kim <thfud333@gmail.com>
2021-08-08 19:17:14 +0900

    모바일 .m_nav 완성 & 스크롤바 숨김 처리 & header 그라디언트 모션 조절
    210808 07:17pm
    - 반응형 : 모바일(767px) main.scss만 남음
    - 스크롤바 기능은 살리되, 스크롤바는 보이지 않도록 (참고 : https://www.w3schools.com/howto/howto_css_hide_scrollbars.asp)
    - Animated Button Gradients (참고 : https://codyhouse.co/nuggets/animated-button-gradients)

--- js/common.js
+++ js/common.js
@@ -1,8 +1,51 @@
 $(document).ready(function () {
   // 네비게이션 클릭시 이동
-  $('.pc_nav ul li a').on('click', function (e) {
+  $('header ul li a').on('click', function (e) {
     e.preventDefault(); // a 태그의 기본 기능 제한!!!
     $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 400, 'swing');
+  });
+
+  // .m_nav 열고 닫기
+  $('.menu_open').on('click', function () {
+    const $menuwrap = $('.menu_wrap');
+    const $first =  $menuwrap.find('.first');
+    const $last =  $menuwrap.find('.last');
+    const $openBtn = $(this);
+
+    // .menu_dim 생성 후 fadeTo로 서서히 나타내기
+    $openBtn.after('<div class="menu_dim"></div>').next().stop().fadeTo('fast', 0.7);
+    // .menu_wrap 우측에서 등장 애니메이션
+    $menuwrap.css('visibility', 'visible').stop().animate({right: 0}, 300, function () {
+      $first.focus(); // 상단 로고로 포커스 강제 이동
+    });
+
+    // .menu_wrap 내 키보드 제어 접근성
+    // 1) $first에서 shift+tab 눌러 이전으로 나가는 경우, $last로 포커스 이동
+    $first.on('keydown', function (e) {
+      if (e.shiftKey && e.keyCode === 9) {
+        e.preventDefault();
+        $last.focus();
+      }
+    })
+    // 2) $last에서 (shift 없이)tab만 눌러 다음으로 나가는 경우, $first로 포커스 이동
+    $last.on('keydown', function (e) {
+      if (!e.shiftKey && e.keyCode === 9) {
+        e.preventDefault();
+        $first.focus();
+      }
+    });
+
+    // .menu_wrap 닫기 (dim, 닫기 버튼, a 눌렀을 때)
+    $('.menu_dim, .menu_close, header ul li a').on('click', function () {
+      $('.menu_dim').stop().fadeTo('slow', 0, function () {
+        $(this).remove(); // .menu_dim을 fadeTo로 서서히 없앤 뒤, 완전히 제거
+      });
+      // .menu_wrap 우측으로 사라지는 애니메이션
+      $menuwrap.stop().animate({right: '-100%'}, 300, function () {
+        $(this).css('visibility', 'hidden');
+        $openBtn.focus(); // 열기 버튼으로 포커스 강제 이동
+      });
+    });
   });
 
   // 사용자가 다크 테마 사용시
